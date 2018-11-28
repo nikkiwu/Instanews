dc9f594d68d48bac312b416c257a11b2f9dde1aa
Nikki Wu <nikkiwu9@gmail.com>
2018-11-28 10:03:03 -0800

    Added document.ready to JQuery

--- js/script.js
+++ js/script.js
@@ -1,74 +1,78 @@
-$('select').selectric();
-
-const $selection = $('.selection');
-const $articleList = $('.contentList');
-const $loader = $('.loader');
+$(document).ready(function () {
 
 
-$loader.hide();
+    $('select').selectric();
+
+    const $selection = $('.selection');
+    const $articleList = $('.contentList');
+    const $loader = $('.loader');
 
 
-$('select').on('change', function () {
-    const section = $(this).val();
+    $loader.hide();
 
-    if (!section.length) {
-        return;
+
+    $('select').on('change', function () {
+        const section = $(this).val();
+
+        if (!section.length) {
+            return;
+        }
+
+        $articleList.empty();
+        $('header').addClass('minified');
+        $('.loader').show();
+
+        getStories(section);
+
+    });
+
+
+    function getStories(section) {
+
+
+        let url = 'https://api.nytimes.com/svc/topstories/v2/';
+        url += section;
+        url += '.json';
+        url += '?' + $.param({'api-key': 'ff0599e4f3a24cf58038240ffc091e3b'});
+
+
+        $.ajax({
+            url: url,
+            method: 'GET',
+        }).done(function (data) {
+            let resultData = '';
+
+            const $dataSet = data.results.filter(function (item) {
+                return item.multimedia.length;
+            }).slice(0, 12);
+
+            for (let value of $dataSet) {
+                const url = value.url;
+                const image = value.multimedia[4].url;
+                const title = value.title;
+                const caption = value.abstract;
+
+
+                resultData += '<a target="_blank" href=' + url + '><li class="articles" alt="' + title + '" style="background-image:url(' + image + ');"><p class="caption">' + caption + '</p></li></a>';
+            }
+
+            $('.loader').hide();
+            $('.contentList').append(resultData);
+        })
+
+            .fail(function () {
+                $('.loader').append('p').text('Something went wrong, sorry!');
+            })
+            .always(function () {
+                $('.loader').hide();
+            });
+
+        $selection.on('click', function () {
+            alert('doesn\'t load');
+        })
+
     }
-
-    $articleList.empty();
-    $('header').addClass('minified');
-    $('.loader').show();
-
-    getStories(section);
 
 });
 
-
-
-function getStories(section) {
-
-
-    let url = 'https://api.nytimes.com/svc/topstories/v2/';
-    url += section;
-    url += '.json';
-    url += '?' + $.param({'api-key': 'ff0599e4f3a24cf58038240ffc091e3b'});
-
-
-
-    $.ajax({
-        url: url,
-        method: 'GET',
-    }).done(function(data) {
-        let resultData = '';
-
-        const $dataSet = data.results.filter(function (item) {
-            return item.multimedia.length;
-        }).slice(0, 12);
-
-        for(let value of $dataSet){
-            const url = value.url;
-            const image = value.multimedia[4].url;
-            const title = value.title;
-            const caption = value.abstract;
-
-
-            resultData += '<a target="_blank" href=' + url + '><li class="articles" alt="'+ title +'" style="background-image:url(' + image + ');"><p class="caption">' + caption + '</p></li></a>';
-        };
-
-        $('.loader').hide();
-        $('.contentList').append(resultData);
-    })
-
-.fail(function() {
-        $('.loader').append('p').text('Something went wrong, sorry!');
-    })
-        .always(function() {
-            $('.loader').hide();
-        });
-
-    $selection.on('click', function () {
-        alert("doesn't load");
-    })
-
-}
 
